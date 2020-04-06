ecde33c4bc0dd88b7e8e2ccee22853102062f12b
Guilherme de Andrade <guilhermedeandrade@protonmail.com>
2020-04-06 21:45:53 +0100

    feat: add PUT /repositories endpoint

--- src/app.js
+++ src/app.js
@@ -1,24 +1,38 @@
-const express = require("express");
+const express = require('express')
 const { uuid } = require('uuidv4')
-const cors = require("cors");
+const cors = require('cors')
 
-const app = express();
+const app = express()
 
-app.use(express.json());
-app.use(cors());
+app.use(express.json())
+app.use(cors())
 
-const repositories = [];
+const repositories = []
 
-app.post("/repositories", (req, res) => {
+app.post('/repositories', (req, res) => {
   const newRepo = { ...req.body, id: uuid(), likes: 0 }
 
   repositories.push(newRepo)
 
   res.status(201).send(newRepo)
-});
+})
 
-app.get("/repositories", (_, res) => {
+app.get('/repositories', (_, res) => {
   res.send(repositories)
-});
+})
 
-module.exports = app;
+app.put('/repositories/:id', (req, res) => {
+  const { id } = req.params
+
+  const repoIndex = repositories.findIndex(repo => repo.id === id)
+
+  if (repoIndex < 0) {
+    res.status(400)
+  }
+
+  const updatedRepo = { ...repositories[repoIndex], ...req.body }
+
+  res.status(201).send(updatedRepo)
+})
+
+module.exports = app
